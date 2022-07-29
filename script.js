52f1d0d464abacd5ac02d22501dd1bc4e77a93a9
Ilona Goldmane <songa17@inbox.lv>
2022-07-29 14:45:53 +0300

    Add html page structure and data to javascript file

---
+++ script.js
@@ -0,0 +1,34 @@
+const solarSystem = document.getElementById("solarSystem");
+const planetsObj = [
+    {
+    "name": "Mercury",
+    "moons": 0
+    },
+    {
+    "name": "Venus",
+    "moons": 0
+    },
+    {
+    "name": "Earth",
+    "moons": 1
+    },
+    {
+    "name": "Mars",
+    "moons": 2
+    },
+    {
+    "name": "Jupiter",
+    "moons": 67
+    },
+    {
+    "name": "Saturn",
+    "moons": 62
+    },
+    {
+    "name": "Uranus",
+    "moons":27
+    },    {
+    "name": "Neptune",
+    "moons": 14
+    }
+];
