45cc5b6e23305b179f9f4e98e1281a73253e2546
Ilona Goldmane <songa17@inbox.lv>
2022-07-31 10:46:22 +0300

    Add base javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const solarSystem = document.getElementById("solarSystem");
+
 const planetsObj = [
     {
     "name": "Mercury",
@@ -32,3 +33,38 @@ const planetsObj = [
     "moons": 14
     }
 ];
+
+let startingNumber = 0;
+let count;
+
+const totalMoons = planetsObj.reduce((acc, planet) => {
+  return acc + planet.moons;
+}, 0);
+
+function displayPlanets(arr){
+    let html = '';
+    return arr.forEach((item) => {
+        html += `
+        <li><span class="bold">${item.name}:</span><span>${item.moons} moons</span></li>
+        `
+       solarSystem.innerHTML = `
+       <ul>${html}</ul>
+        <p class="bold total-moons-p">total moons in the solar system: <span id="total">${totalMoons}<span><p>
+       `
+    });
+}
+
+displayPlanets(planetsObj);
+
+function countMoons() {
+  document.getElementById("total").textContent = startingNumber;
+  startingNumber++;
+  if (startingNumber === totalMoons + 1) {
+    clearInterval(count);
+  }
+}
+
+count = setInterval(countMoons, 10);
+
+
+
